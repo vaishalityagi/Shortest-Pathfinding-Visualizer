2fed1d10f948418990ae2b75b96fae3e38bffc51
Vaishali Tyagi <tyagivaishali12341@gmail.com>
2025-01-31 09:54:53 +0530

    Changing cogigurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'0.0.0.0',
-    // port: process. ENV.PORT ?parseInt(ProcessingInstruction.env.PORT):5173,
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // Change if needed
   }
+ 
 })
